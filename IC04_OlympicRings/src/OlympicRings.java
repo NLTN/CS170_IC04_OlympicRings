fbb75795454502c8a194f0f16df45cfa284510dd
Nguyen Nguyen <nltn@msn.com>
2018-02-11 02:16:11 -0800

    Clean up & Add comments

--- IC04_OlympicRings/src/OlympicRings.java
+++ IC04_OlympicRings/src/OlympicRings.java
@@ -22,11 +22,17 @@ public class OlympicRings extends JApplet {
 	private static final long serialVersionUID = 6080844738077899107L;
 
 	public void init() {
-		setSize(500, 300);
+		// Set the canvas size.
+		setSize(600, 300);
 	}
 
 	public void paint(Graphics canvas) {
-		DrawOlympicRings(canvas);
+		final Point startingPosition = new Point(20, 20);
+		final int radius = 80;
+		final int gap = 20;
+		final short stroke = 10;
+		
+		drawOlympicRings(startingPosition, radius, gap, stroke, canvas);
 		/*
 		 * // Variables int diameter = 120, gap = 20, stroke = 10; Point beginPos = new
 		 * Point(0, 0);
@@ -50,38 +56,35 @@ public class OlympicRings extends JApplet {
 		 * (diameter / 2) + 1 * (diameter + gap + stroke), beginPos.y + diameter / 2,
 		 * diameter, diameter);
 		 */
-
-		// Test 1
-		// canvas.drawArc(beginPos.x, beginPos.y, diameter, diameter, 0,
-		// (360-stroke*2));
-
-		// Test 2
-		// canvas.drawLine(60, 60, 60, 60);
-		// canvas.drawLine(210, 60, 210, 60);
-		// canvas.drawLine(360, 60, 360, 60);
-
-		// System.out.println(beginPos.x + diameter / 2);
-		// System.out.println(beginPos.x + 1 * (diameter + gap + stroke) + diameter /
-		// 2);
-		// System.out.println(beginPos.x + 2 * (diameter + gap + stroke) + diameter /
-		// 2);
-
-		
 	}
 
-	public void DrawOlympicRings(Graphics canvas) {
-		// Variables - General variables
-		int radius = 60, gap = 20, stroke = 10;
-		Point beginPos = new Point(10, 10);
+	public double getCutoutPosition(Ring target, Ring source) {
+		// Variables
+		Trigonometry.Circle.IntersectionPoints iPoints;
+		double angle;
 
+		// Calculate - Yellow Ring cut out angle
+		iPoints = Trigonometry.Circle.getIntersectionPoints(target, source);
+		angle = Math.atan2(iPoints.Point2.getY() - target.Center.getY(), iPoints.Point2.getX() - target.Center.getX());
+
+		return -Trigonometry.Circle.convertRadianToDegree(angle);
+	}
+
+	/**
+	 * Draw the Olympic Rings to the canvas.
+	 */
+	public void drawOlympicRings(Point startingPosition, int radius, int gap, int stroke, Graphics canvas) {
 		// Variables - Positions
-		Point blueRingCenter = new Point(beginPos.x + 0 * (radius * 2 + gap + stroke) + radius, beginPos.y + radius);
-		Point blackRingCenter = new Point(beginPos.x + 1 * (radius * 2 + gap + stroke) + radius, beginPos.y + radius);
-		Point redRingCenter = new Point(beginPos.x + 2 * (radius * 2 + gap + stroke) + radius, beginPos.y + radius);
-		Point yellowRingCenter = new Point(beginPos.x + gap / 2 + radius * 2 + 0 * (radius * 2 + gap + stroke),
-				beginPos.y + radius * 2);
-		Point greenRingCenter = new Point(beginPos.x + gap / 2 + radius * 2 + 1 * (radius * 2 + gap + stroke),
-				beginPos.y + radius * 2);
+		Point blueRingCenter = new Point(startingPosition.x + 0 * (radius * 2 + gap + stroke) + radius,
+				startingPosition.y + radius);
+		Point blackRingCenter = new Point(startingPosition.x + 1 * (radius * 2 + gap + stroke) + radius,
+				startingPosition.y + radius);
+		Point redRingCenter = new Point(startingPosition.x + 2 * (radius * 2 + gap + stroke) + radius,
+				startingPosition.y + radius);
+		Point yellowRingCenter = new Point(startingPosition.x + gap / 2 + radius * 2 + 0 * (radius * 2 + gap + stroke),
+				startingPosition.y + radius * 2);
+		Point greenRingCenter = new Point(startingPosition.x + gap / 2 + radius * 2 + 1 * (radius * 2 + gap + stroke),
+				startingPosition.y + radius * 2);
 
 		// Variables - Olympic rings
 		Ring blueRing = new Ring(blueRingCenter, radius, stroke, Color.BLUE);
@@ -89,34 +92,14 @@ public class OlympicRings extends JApplet {
 		Ring blackRing = new Ring(blackRingCenter, radius, stroke, Color.BLACK);
 		Ring redRing = new Ring(redRingCenter, radius, stroke, Color.RED);
 		Ring greenRing = new Ring(greenRingCenter, radius, stroke, Color.GREEN);
-		
-		
-		// Variables - Intersection
-		Trigonometry.Circle.IntersectionPoints iPoints;
-		double angle;
-		
-		// Calculate - Yellow Ring cut out angle
-		iPoints = Trigonometry.Circle.GetIntersectionPoints(yellowRing, blueRing);
-		angle = Math.atan2(iPoints.Point2.getY() - yellowRing.Center.getY(), iPoints.Point2.getX() - yellowRing.Center.getX());
-		yellowRing.setCutOutPosition(-(int)Trigonometry.Circle.RadianToDegree(angle));
-		
-		// Calculate - Black Ring cut out angle
-		iPoints = Trigonometry.Circle.GetIntersectionPoints(blackRing,yellowRing);
-		angle = Math.atan2(iPoints.Point2.getY() - blackRing.Center.getY(), iPoints.Point2.getX() - blackRing.Center.getX());
-		blackRing.setCutOutPosition(-(int)Trigonometry.Circle.RadianToDegree(angle));
-		System.out.println("angle1=" + (int)Trigonometry.Circle.RadianToDegree(angle));
-		
-		// Calculate - Green Ring cut out angle
-		iPoints = Trigonometry.Circle.GetIntersectionPoints(greenRing,blackRing);
-		angle = Math.atan2(iPoints.Point2.getY() - greenRing.Center.getY(), iPoints.Point2.getX() - greenRing.Center.getX());
-		greenRing.setCutOutPosition(-(int)Trigonometry.Circle.RadianToDegree(angle));
-		
-		// Calculate - Red Ring cut out angle
-		iPoints = Trigonometry.Circle.GetIntersectionPoints(redRing,greenRing);
-		angle = Math.atan2(iPoints.Point2.getY() - redRing.Center.getY(), iPoints.Point2.getX() - redRing.Center.getX());
-		redRing.setCutOutPosition(-(int)Trigonometry.Circle.RadianToDegree(angle));
-				
-		// Draw to canvas 
+
+		// Set Cut-out position
+		yellowRing.setCutOutPosition((int) getCutoutPosition(yellowRing, blueRing));
+		blackRing.setCutOutPosition((int) getCutoutPosition(blackRing, yellowRing));
+		greenRing.setCutOutPosition((int) getCutoutPosition(greenRing, blackRing));
+		redRing.setCutOutPosition((int) getCutoutPosition(redRing, greenRing));
+
+		// Draw to canvas
 		blueRing.draw(canvas);
 		yellowRing.draw(canvas);
 		blackRing.draw(canvas);
@@ -131,6 +114,7 @@ public class OlympicRings extends JApplet {
 		public Color RingColor;
 		public int CutOutAngle = -999;
 
+		// Constructor
 		public Ring(Point centerPoint, int radius, int stroke, Color color) {
 			Center = centerPoint;
 			Radius = radius;
@@ -138,23 +122,31 @@ public class OlympicRings extends JApplet {
 			Stroke = stroke;
 		}
 
+		/**
+		 * Set the angle at which the circle will be cut out.
+		 */
 		public void setCutOutPosition(int angle) {
 			CutOutAngle = angle;
 		}
 
+		/**
+		 * Draw the circle to the canvas.
+		 */
 		public void draw(Graphics canvas) {
-			// Set Stroke
+			// Set stroke
 			Graphics2D canvas2D = (Graphics2D) canvas;
 			canvas2D.setStroke(new BasicStroke(Stroke));
 
+			// Set color
 			canvas.setColor(RingColor);
+
+			// Draw
 			if (CutOutAngle != -999) {
 				canvas.drawArc(Center.x - Radius, Center.y - Radius, (Radius * 2), (Radius * 2), (CutOutAngle + Stroke),
 						360 - Stroke * 2);
 			} else {
 				canvas.drawOval(Center.x - Radius, Center.y - Radius, Radius * 2, Radius * 2);
 			}
-
 		}
 	}
 
@@ -164,16 +156,25 @@ public class OlympicRings extends JApplet {
 				public Point Point1 = new Point(), Point2 = new Point();
 			}
 
-			public static double RadianToDegree(double angle) {
-				return angle*180/Math.PI;
+			/**
+			 * Convert Radian to Degree. Return a {@code double} value.
+			 */
+			public static double convertRadianToDegree(double angle) {
+				return angle * 180 / Math.PI;
 			}
-			
-			public static IntersectionPoints GetIntersectionPoints(Ring ring1, Ring ring2) {
+
+			// https://math.stackexchange.com/a/1367732
+			/**
+			 * Return the {@code IntersectionPoints} at which two circles intersect. 
+			 * This method is based on the math https://math.stackexchange.com/a/1367732 by
+			 * johannesvalks.
+			 */
+			public static IntersectionPoints getIntersectionPoints(Ring ring1, Ring ring2) {
+				// Variables
 				IntersectionPoints points = new IntersectionPoints();
 
 				// R = The distance between the centers of two circles
-				double R = Trigonometry.Circle.DistanceBetweenTheCenterTwoCircles(ring1.Center.x, ring2.Center.x,
-						ring1.Center.y, ring2.Center.y);
+				double R = Trigonometry.Circle.getDistanceBetweenTheCentersOfTwoCircles(ring1, ring2);
 
 				double x1 = ring1.Center.x;
 				double x2 = ring2.Center.x;
@@ -184,42 +185,53 @@ public class OlympicRings extends JApplet {
 				double fx = (x1 + x2) / 2
 						+ ((Math.pow(ring1.Radius, 2) - Math.pow(ring2.Radius, 2)) / (2 * Math.pow(R, 2))) * (x2 - x1);
 
-				
-
 				// gx
-				double gx = 		0.5 * 
-								Math.sqrt(
-											(2 * (Math.pow(ring1.Radius, 2) + Math.pow(ring2.Radius, 2)) /Math.pow(R, 2)) 
-											- (Math.pow(Math.pow(ring1.Radius, 2) - Math.pow(ring2.Radius, 2), 2))/Math.pow(R, 4) - 1 
-										) * (y2 - y1);
+				double gx = 0.5
+						* Math.sqrt((2 * (Math.pow(ring1.Radius, 2) + Math.pow(ring2.Radius, 2)) / Math.pow(R, 2))
+								- (Math.pow(Math.pow(ring1.Radius, 2) - Math.pow(ring2.Radius, 2), 2)) / Math.pow(R, 4)
+								- 1)
+						* (y2 - y1);
 				// fy
-				double fy = (y1+y2)/2 + (Math.pow(ring1.Radius, 2) - Math.pow(ring2.Radius, 2))/(2 * Math.pow(R, 2)) * (y2-y1);
+				double fy = (y1 + y2) / 2
+						+ (Math.pow(ring1.Radius, 2) - Math.pow(ring2.Radius, 2)) / (2 * Math.pow(R, 2)) * (y2 - y1);
 
 				// gy
-				double gy = 0.5 * 
-						Math.sqrt(
-								(2 * (Math.pow(ring1.Radius, 2) + Math.pow(ring2.Radius, 2)) /Math.pow(R, 2)) 
-								- (Math.pow(Math.pow(ring1.Radius, 2) - Math.pow(ring2.Radius, 2), 2))/Math.pow(R, 4) - 1 
-							) * (x1 - x2);
-				
+				double gy = 0.5
+						* Math.sqrt((2 * (Math.pow(ring1.Radius, 2) + Math.pow(ring2.Radius, 2)) / Math.pow(R, 2))
+								- (Math.pow(Math.pow(ring1.Radius, 2) - Math.pow(ring2.Radius, 2), 2)) / Math.pow(R, 4)
+								- 1)
+						* (x1 - x2);
+
+				points.Point1.setLocation(fx + gx, fy + gy);
+				points.Point2.setLocation(fx - gx, fy - gy);
+
+				// Print to console
 				System.out.println("fx=" + fx);
 				System.out.println("gx=" + gx);
 				System.out.println("fy=" + fy);
 				System.out.println("gy=" + gy);
-				
-				points.Point1.setLocation(fx + gx, fy + gy);
-				points.Point2.setLocation(fx - gx, fy - gy);
-				
 				System.out.println("(x1, y1)=" + points.Point1.getX() + "," + points.Point1.getY());
 				System.out.println("(x2, y2)=" + points.Point2.getX() + "," + points.Point2.getY());
+
+				// Return
 				return points;
 			}
 
-			public static double DistanceBetweenTheCenterTwoCircles(double x1, double x2, double y1, double y2) {
+			/**
+			 * Return the distance between the centers of two circles as {@code double} value.
+			 * This method is based on the math https://math.stackexchange.com/a/1367732 by
+			 * johannesvalks.
+			 */
+			public static double getDistanceBetweenTheCentersOfTwoCircles(Ring ring1, Ring ring2) {
+				// R = Square of (x2-x1)^2 + (y2 - y1)^2
+				return Math.sqrt(Math.pow((ring2.Center.getX() - ring1.Center.getX()), 2)
+						+ Math.pow((ring2.Center.getY() - ring1.Center.getY()), 2));
+			}
+
+			public static double getDistanceBetweenTheCentersOfTwoCircles(double x1, double x2, double y1, double y2) {
 				// R = Square of (x2-x1)^2 + (y2 - y1)^2
 				return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
 			}
 		}
-
 	}
 }
